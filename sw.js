202dad62c581df1c3c93a413eeb13afeb50b2801
tkandziora <54751374+tkandziora@users.noreply.github.com>
2026-03-26 15:50:31 -0400

    Add files via upload
    release 4.1.2

--- sw.js
+++ sw.js
@@ -1,20 +1,38 @@
-const CACHE_NAME = 'fitness-v4.1-cache';
-const ASSETS = [
+// Incremented version to force a fresh cache update for v4.1.1
+const CACHE_NAME = 'fitness-coach-v4.1.1';
+const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-// Install and cache assets
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+// Install Event: Logic to save files locally
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching app shell and content');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
-// Serve from cache
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// Activate Event: Clean up old versions of the app
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event: Serve the app from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
